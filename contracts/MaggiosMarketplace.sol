f780aaa3024efe07502f722b1bd34e4258f0773c
tyler <tymaggio03@gmail.com>
2022-06-05 16:15:38 -0600

    Add function for fetching user NFTs, add function for fetching market items

--- contracts/MaggiosMarketplace.sol
+++ contracts/MaggiosMarketplace.sol
@@ -121,4 +121,56 @@ contract MaggiosMarketplace is ERC721URIStorage {
         // Send eth to seller
         payable(idToMarketItem[tokenId].seller).transfer(msg.value);
     }
+    // Returns all unsold items belonging to marketplace
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _tokenIds.current();
+        uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+        uint currentIndex= 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+
+        for(uint i = 0; i < itemCount; i++) {
+            // Check whether item is unsold
+            if(idToMarketItem[i + 1].owner == address(this)) {
+                uint currentId = i + 1;
+
+                MarketItem storage currentItem = idToMarketItem[currentId];
+
+                items[currentIndex] = currentItem;
+
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+        // Find how many NFTs user owns
+        for(uint i = 0; i < totalItemCount; i++) {
+            if(idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+
+        for(uint i = 0; i < totalItemCount; i++) {
+            // Check whether item is unsold
+            if(idToMarketItem[i + 1].owner == msg.sender) {
+                uint currentId = i + 1;
+
+                MarketItem storage currentItem = idToMarketItem[currentId];
+
+                items[currentIndex] = currentItem;
+
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
 }
